bfea651aa0e7032a4bfa48cf06e8ad497e70c1f0
IAmFiveHigh <745170149@qq.com>
2017-05-26 14:58:13 +0800

    添加数字转中文字符串

---
+++ ToolProject/Tool/Extension/Int+extension.swift
@@ -0,0 +1,63 @@
+//
+//  Int+extension.swift
+//  AgricultureGeek
+//
+//  Created by 每天农资 on 2017/5/26.
+//  Copyright © 2017年 我是五高你敢信. All rights reserved.
+//
+
+import Foundation
+
+//extension Int {
+//    
+////    static func stringWithInt(_ num: Int) -> String {
+////        
+////        let formatter = NumberFormatter()
+////        formatter.numberStyle = .
+////    }
+//    
+//}
+
+extension Int {
+    var cn: String {
+        get {
+            if self == 0 {
+                return "零"
+            }
+            var zhNumbers = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"]
+            var units = ["", "十", "百", "千", "万", "十", "百", "千", "亿", "十","百","千"]
+            var cn = ""
+            var currentNum = 0
+            var beforeNum = 0
+            let intLength = Int(floor(log10(Double(self))))
+            for index in 0...intLength {
+                currentNum = self/Int(pow(10.0,Double(index)))%10
+                if index == 0{
+                    if currentNum != 0 {
+                        cn = zhNumbers[currentNum]
+                        continue
+                    }
+                } else {
+                    beforeNum = self/Int(pow(10.0,Double(index-1)))%10
+                }
+                if [1,2,3,5,6,7,9,10,11].contains(index) {
+                    if currentNum == 1 && [1,5,9].contains(index) && index == intLength { // 处理一开头的含十单位
+                        cn = units[index] + cn
+                    } else if currentNum != 0 {
+                        cn = zhNumbers[currentNum] + units[index] + cn
+                    } else if beforeNum != 0 {
+                        cn = zhNumbers[currentNum] + cn
+                    }
+                    continue
+                }
+                if [4,8,12].contains(index) {
+                    cn = units[index] + cn
+                    if (beforeNum != 0 && currentNum == 0) || currentNum != 0 {
+                        cn = zhNumbers[currentNum] + cn
+                    }
+                }
+            }
+            return cn
+        }
+    }
+}
