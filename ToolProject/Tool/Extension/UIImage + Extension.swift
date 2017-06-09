044efb632e73439b4013e8667193ed1daa478b30
IAmFiveHigh <745170149@qq.com>
2017-06-09 15:31:53 +0800

    添加扣图UIImage的Extension

---
+++ ToolProject/Tool/Extension/UIImage + Extension.swift
@@ -0,0 +1,41 @@
+//
+//  UIImage + Extension.swift
+//  ToolProject
+//
+//  Created by 每天农资 on 2017/6/9.
+//  Copyright © 2017年 我是五高你敢信. All rights reserved.
+//
+
+import Foundation
+
+extension UIImage {
+    
+    //返回一个将白色背景变透明的UIImage
+    func imageByRemoveWhiteBg() -> UIImage? {
+        let colorMasking: [CGFloat] = [222, 255, 222, 255, 222, 255]
+        return transparentColor(colorMasking: colorMasking)
+    }
+    
+    //返回一个将黑色背景变透明的UIImage
+    func imageByRemoveBlackBg() -> UIImage? {
+        let colorMasking: [CGFloat] = [0, 32, 0, 32, 0, 32]
+        return transparentColor(colorMasking: colorMasking)
+    }
+    
+    func transparentColor(colorMasking:[CGFloat]) -> UIImage? {
+        if let rawImageRef = self.cgImage {
+            UIGraphicsBeginImageContext(self.size)
+            if let maskedImageRef = rawImageRef.copy(maskingColorComponents: colorMasking) {
+                let context: CGContext = UIGraphicsGetCurrentContext()!
+                context.translateBy(x: 0.0, y: self.size.height)
+                context.scaleBy(x: 1.0, y: -1.0)
+                context.draw(maskedImageRef, in: CGRect(x:0, y:0, width:self.size.width,
+                                                        height:self.size.height))
+                let result = UIGraphicsGetImageFromCurrentImageContext()
+                UIGraphicsEndImageContext()
+                return result
+            }
+        }
+        return nil
+    }
+}
